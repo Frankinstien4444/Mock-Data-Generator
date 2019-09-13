692086be349eeb39fb8bc4616b5194e50eb3af38
Frankinstien4444 <55277259+Frankinstien4444@users.noreply.github.com>
2019-09-13 03:26:06 -0700

    Add files via upload

---
+++ Interfaces/IDataRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using MockDataGenerator.Enums;
+
+namespace MockDataGenerator.Interfaces
+{
+    
+    public interface IDataRule
+    {
+        [DataMember]
+        String Description { set; get; }
+        [DataMember]
+        FieldTypes RuleType { set; get; }
+        [DataMember]
+        bool IsRandom { set; get; }
+        [DataMember]
+        String FieldName { set; get; }
+        [DataMember]
+        String FieldDataType { set; get; }
+        [DataMember]
+        bool ConvertToString { set; get; }
+        [DataMember]
+        Guid RuleId { set; get; }
+    }
+}
---
+++ Interfaces/IRuleControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MockDataGenerator.SpecialObjects;
+
+namespace MockDataGenerator.Interfaces
+{
+    public interface IRuleControl
+    {
+       CommonCtrlProperies Properties { set; get; }
+       bool ApplyDataToRuleSet();
+       void EditRule(IDataRule aRue);
+    }
+}

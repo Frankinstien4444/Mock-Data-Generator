ad63851732b31cf327a2b7ecd5471f15de2a2fd2
Frankinstien4444 <55277259+Frankinstien4444@users.noreply.github.com>
2019-09-13 03:31:10 -0700

    Add files via upload

---
+++ SpecialObjects/Rules/RuleSupport/DataField.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockDataGenerator.SpecialObjects.Rules.RuleSupport
+{
+    [Serializable]
+    public class DataField
+    {
+        /// <summary>
+        /// represents the data source column or treenode
+        /// </summary>
+        [DataMember]
+        public String ColumnName { set; get; }
+        /// <summary>
+        /// represents a field, parameter or property of an object
+        /// </summary>
+        [DataMember]
+        public String FieldName { set; get; }
+
+        public DataField()
+        {
+
+        }
+
+        public DataField(string column)
+        {
+            ColumnName = column;
+        }
+    }
+}
---
+++ SpecialObjects/Rules/RuleSupport/SearchField.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using MockDataGenerator.Enums;
+
+namespace MockDataGenerator.SpecialObjects.Rules.RuleSupport
+{
+    [Serializable]
+    public class SearchField
+    {
+        /// <summary>
+        /// represents the data source column or treenode
+        /// </summary>
+        [DataMember]
+        public string ColumnName { set; get; }
+        [DataMember]
+        public LogicTypes Logic { set; get; }
+        [DataMember]        
+        public String RangeStart { set; get; }
+        [DataMember]
+        public String RangeEnd { set; get; }
+        [DataMember]
+        public String RegularExpression { set; get; }
+        [DataMember]
+        public Boolean IsRandom { set; get; }
+
+        public SearchField()
+        {
+
+        }
+
+        public SearchField(string column)
+        {
+            ColumnName = column;
+        }
+
+        public SearchField(string column, LogicTypes logic)
+        {
+            ColumnName = column;
+            Logic = logic;
+        }
+    }
+}
